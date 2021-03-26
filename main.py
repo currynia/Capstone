e6e7e87c83341272b7990920e4752cda9f830449
TWenRui <teewenrui31@gmail.com>
2021-03-26 21:13:08 +0800

    git c

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sqlite3
 from datastore import dataStore
 from dataread import reader
-from flask import Flask,request,rende_template
+from flask import Flask,request,render_template
 
 datastore = dataStore()
 datastore.connect()
@@ -12,4 +12,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return render_template()
+    return render_template('index.html')
+
+@app.route('/nearest', methods=['POST','GET'])
+def nearest():
+    return render_template('nearest.html')
+
+@app.route('/nearest1', methods=['POST','GET'])
+def nearest1():
+    print(request.form['id'])
+    return ('''asdasd''')
+
+
+
+app.run(debug=True)
