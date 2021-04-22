b276d4cac7d6d779a57a014bbe231c0a6932dcf9
Replit user <replituser@example.com>
2021-04-22 08:08:24 +0000

    Added form validation

--- main.py
+++ main.py
@@ -24,22 +24,34 @@ def root():
 @app.route('/nearestbusstop', methods=['GET'])
 def nearest():
     try:
-        #get latitude and longitude from form and stores it into a dictionary
         lat = request.args['Lat']
         long = request.args['Long']
-        location = {'lat':float(lat),'long':float(long)}
         submission_successful = True
-        stops = datastore.get_records('bus.db','get_coord')
-        distance_data = [] #initialise empty array
-        for stop in stops: #calculate distance of each bus stop from coordinates
-            distance_data.append({'BusstopCode':stop[0],'Description':stop[1],'distance':formula.haversine(location['lat'],location['long'],stop[2],stop[3])})
-        distance_data = sortalgo.sort_distance(distance_data) #sort array based on distance from coordinates
-        for distance in distance_data: #convert distance values into 2dp
-            distance.update({'distance':round(distance['distance'],2)})
-        return render_template('nearest.html',submission_successful=submission_successful,distance_data = distance_data)
-    
     except:
-        
+        submission_successful = False
+    
+    if submission_successful:
+        #get latitude and longitude from form and stores it into a dictionary
+        lat_check = 'None'
+        long_check = 'None'
+        if lat.count('.') == 1 and long.count('.') == 1:
+            lat_check,long_check = lat.replace('.',''),long.replace('.','')
+        if lat_check.isdigit() and long_check.isdigit(): 
+            location = {'lat':float(lat),'long':float(long)}
+            submission_successful = True
+            stops = datastore.get_records('bus.db','get_coord')
+            distance_data = [] 
+            for stop in stops: #calculate distance of each bus stop from coordinates
+                distance_data.append({'BusstopCode':stop[0],'Description':stop[1],'distance':formula.haversine(location['lat'],location['long'],stop[2],stop[3])})
+            distance_data = sortalgo.sort_distance(distance_data) #sort array based on distance from specified coordinates
+            for distance in distance_data: #convert distance values into 2dp
+                distance.update({'distance':round(distance['distance'],2)})
+            return render_template('nearest.html',submission_successful=submission_successful,distance_data = distance_data)
+    
+        else:
+            error = "Please input numbers only"
+            return render_template('nearest.html',error=error)
+    else:
         return render_template('nearest.html')
 
 @app.route('/help')
